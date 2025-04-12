f1cf2cfe886a6c549a46b8610d3f8e20f330565f
dizon-kevin <ashleynicolegutierrez7@gmail.com>
2025-04-12 22:03:15 +0800

    commit by kevin

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'variables.dart';
+
 
 class Contact extends StatefulWidget {
   const Contact({super.key});
@@ -13,99 +15,141 @@ class _ContactState extends State<Contact> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
 
-        ),
         child: SafeArea(child:Column(
       children: [
 
-        Image.network(photo, width: double.infinity, fit: BoxFit.fill, height: 300,),
-        Text(name, style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(15,0,15,10),
-                  padding: EdgeInsets.fromLTRB(15,0,15,10),
-                  decoration: BoxDecoration(
-                      color: CupertinoColors.systemGrey.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Column(
+        Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+
+            Image.network(photo, width: double.infinity, fit: BoxFit.fill, height: 300,),
+            Positioned(
+                  top: -2,
+                left: -15,
+                child: Row(
+              children: [
+                CupertinoButton(child: Icon(CupertinoIcons.chevron_back, color: CupertinoColors.white,), onPressed: (){
+                  Navigator.pop(context);
+                })
+              ],
+
+            )),
+            Positioned(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      CupertinoButton(child: Icon(CupertinoIcons.bubble_middle_bottom_fill, color: CupertinoColors.white,), onPressed: (){
-
-                      }),
-                      Text('Message', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
-
+              
+                      Text("last used: ", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w200),),
+                      Container(
+                          padding: EdgeInsets.fromLTRB(5,0,5,0),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              color: CupertinoColors.white
+                          ),
+                          child: Text("P", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: CupertinoColors.black),)),
+                      Text("Primary", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w200),),
+              
+                      Icon(CupertinoIcons.chevron_forward, size: 12, color: CupertinoColors.white,)
                     ],
                   ),
-                ),
+                  Text(name, style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(5,0,5,0),
+                            padding: EdgeInsets.fromLTRB(10,0,10,5),
+                            decoration: BoxDecoration(
+                                color: CupertinoColors.systemGrey.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Column(
+                              children: [
+                                CupertinoButton(child: Icon(CupertinoIcons.bubble_middle_bottom_fill, color: CupertinoColors.white,), onPressed: () async{
+                                    final Uri uri = await Uri.parse('sms: $phone');
+                                    await launchUrl(uri);
+                                }),
+                                Text('Message', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
+              
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(5,0,5,0),
+                            padding: EdgeInsets.fromLTRB(10,0,10,5),
+                            decoration: BoxDecoration(
+                                color: CupertinoColors.systemGrey.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Column(
+                              children: [
+                                CupertinoButton(child: Icon(CupertinoIcons.phone_solid, color: CupertinoColors.white,), onPressed: () async{
+                                  final Uri uri = await Uri.parse('tel: $phone');
+                                  await launchUrl(uri);
+                                }),
+                                Text('Call', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
+              
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(5,0,5,0),
+                            padding: EdgeInsets.fromLTRB(10,0,10,5),
+                            decoration: BoxDecoration(
+                                color: CupertinoColors.systemGrey.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Column(
+                              children: [
+                                CupertinoButton(child: Icon(CupertinoIcons.video_camera_solid, color: CupertinoColors.white,), onPressed: (){
+
+                                }),
+                                Text('Video', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
+              
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(5,0,5,0),
+                            padding: EdgeInsets.fromLTRB(10,0,10,5),
+                            decoration: BoxDecoration(
+                                color: CupertinoColors.systemGrey.withOpacity(0.2),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Column(
+                              children: [
+                                CupertinoButton(child: Icon(CupertinoIcons.mail_solid, color: CupertinoColors.white,), onPressed: () async{
+                                  final Uri uri = await Uri.parse('email: $email');
+                                  await launchUrl(uri);
+                                }),
+                                Text('Mail', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
+              
+                              ],
+                            ),
+                          ),
+                        ),
+              
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(15,0,15,10),
-                  padding: EdgeInsets.fromLTRB(15,0,15,10),
-                  decoration: BoxDecoration(
-                      color: CupertinoColors.systemGrey.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Column(
-                    children: [
-                      CupertinoButton(child: Icon(CupertinoIcons.phone_solid, color: CupertinoColors.white,), onPressed: (){
+            )
+          ],
+        ),
 
-                      }),
-                      Text('Call', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
-
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(15,0,15,10),
-                  padding: EdgeInsets.fromLTRB(15,0,15,10),
-                  decoration: BoxDecoration(
-                      color: CupertinoColors.systemGrey.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Column(
-                    children: [
-                      CupertinoButton(child: Icon(CupertinoIcons.bubble_middle_bottom_fill, color: CupertinoColors.white,), onPressed: (){
-
-                      }),
-                      Text('Message', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
-
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(15,0,15,10),
-                  padding: EdgeInsets.fromLTRB(15,0,15,10),
-                  decoration: BoxDecoration(
-                      color: CupertinoColors.systemGrey.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(10)
-                  ),
-                  child: Column(
-                    children: [
-                      CupertinoButton(child: Icon(CupertinoIcons.bubble_middle_bottom_fill, color: CupertinoColors.white,), onPressed: (){
-
-                      }),
-                      Text('Message', style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),)
-
-                    ],
-                  ),
-                ),
-              ),
-
-            ],
-          ),
-        )
       ],
     )));
   }
