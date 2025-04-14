f26b5d3771f6571e0a2c3438365438a81a6b2cfe
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-04-14 15:58:37 +0800

    commit by marc

--- lib/main.dart
+++ lib/main.dart
@@ -96,6 +96,128 @@ class _HomepageState extends State<Homepage> {
     }
   }
 
+  void _deleteContact(int index) {
+    int actualIndex = contacts.indexOf(filteredContacts[index]);
+    if (actualIndex != -1) {
+      setState(() {
+        contacts.removeAt(actualIndex);
+        filteredContacts.removeAt(index);
+        _myBox.put('contacts', contacts);
+      });
+    }
+  }
+
+  Future<void> _pickImage() async {
+    try {
+      final XFile? image = await _picker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 75, // Reduce quality to save storage space
+      );
+
+      if (image != null) {
+        final bytes = await image.readAsBytes();
+        setState(() {
+          // Convert image to base64 string for storage
+          _selectedImageBase64 = base64Encode(bytes);
+        });
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
+  // Method to get image widget from base64 string
+  Widget _getImageWidget(String? base64String) {
+    if (base64String != null && base64String.isNotEmpty) {
+      try {
+        return Image.memory(
+          base64Decode(base64String),
+          fit: BoxFit.cover,
+          width: 200,
+          height: 200,
+        );
+      } catch (e) {
+        print('Error decoding image: $e');
+      }
+    }
+
+    // Return default icon if no image or error
+    return Icon(
+      CupertinoIcons.person_circle_fill,
+      color: CupertinoColors.systemGrey,
+      size: 200,
+    );
+  }
+
+  TextEditingController _fname = TextEditingController();
+  TextEditingController _lname = TextEditingController();
+  TextEditingController _company = TextEditingController();
+  TextEditingController _url = TextEditingController();
+
+  // List to store phone fields with their labels
+  List<PhoneField> _phoneFields = [PhoneField()];
+  // List to store email fields with their labels
+  List<EmailField> _emailFields = [EmailField()];
+
+  void _addPhoneField() {
+    setState(() {
+      _phoneFields.add(PhoneField());
+    });
+  }
+// eric
+  void _addEmailField() {
+    setState(() {
+      _emailFields.add(EmailField());
+    });
+  }
+
+  // List of available phone labels
+  final List<String> _phoneLabels = ['mobile', 'home', 'work', 'school', 'iPhone', 'Apple Watch', 'main', 'home fax', 'work fax', 'pager', 'other'];
+  // List of available email labels
+  final List<String> _emailLabels = ['home', 'work', 'school', 'iCloud', 'other'];
+
+  void _openContactSheet({Map<String, dynamic>? contactToEdit, int? editIndex}) {
+    // Reset or fill with contact data
+    if (contactToEdit != null) {
+      _fname.text = contactToEdit['name'].split(" ")[0] ?? "";
+      _lname.text = contactToEdit['name'].split(" ").length > 1 ? contactToEdit['name'].split(" ")[1] : "";
+      _company.text = contactToEdit['company'] ?? "";
+      _url.text = contactToEdit['url'] ?? "";
+      _selectedImageBase64 = contactToEdit['isBase64'] ? contactToEdit['photo'] : null;
+
+      // Setup phone fields
+      _phoneFields.clear();
+      if (contactToEdit['phoneNumbers'] != null && contactToEdit['phoneNumbers'].isNotEmpty) {
+        for (var phone in contactToEdit['phoneNumbers']) {
+          PhoneField field = PhoneField(initialLabel: phone['label']);
+          field.controller.text = phone['number'];
+          _phoneFields.add(field);
+        }
+      } else {
+        _phoneFields.add(PhoneField());
+      }
+
+      // Setup email fields
+      _emailFields.clear();
+      if (contactToEdit['emailAddresses'] != null && contactToEdit['emailAddresses'].isNotEmpty) {
+        for (var email in contactToEdit['emailAddresses']) {
+          EmailField field = EmailField(initialLabel: email['label']);
+          field.controller.text = email['email'];
+          _emailFields.add(field);
+        }
+      } else {
+        _emailFields.add(EmailField());
+      }
+    } else {
+      // Reset for new contact
+      _selectedImageBase64 = null;
+      _phoneFields = [PhoneField()];
+      _emailFields = [EmailField()];
+      _fname.clear();
+      _lname.clear();
+      _company.clear();
+      _url.clear();
+    }
 
   @override
   Widget build(BuildContext context) {
