1962ba2469e29f722077264a564d84d0dcaa12a7
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-04-14 16:21:28 +0800

    commit by marc

--- lib/main.dart
+++ lib/main.dart
@@ -742,129 +742,38 @@ setState(() {
   }
 
 
+   @override
+  void dispose() {
+    _fname.dispose();
+    _lname.dispose();
+    _company.dispose();
+    _url.dispose();
+    _searchController.dispose();
+    for (var field in _phoneFields) {
+      field.controller.dispose();
+    }
+    for (var field in _emailFields) {
+      field.controller.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
-        trailing: CupertinoButton(
-          child: Icon(CupertinoIcons.add),
-          onPressed: () {
-            showCupertinoModalPopup(
-              context: context,
-              builder: (context) {
-                return CupertinoActionSheet(
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CupertinoButton(
-                        child: Text('Cancel'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      Text('New Contact'),
-                      CupertinoButton(
-                        child: Text('Done'),
-                        onPressed: () {
-                          setState(() {
-                            contacts.add({
-                              "name": _fname.text + " " + _lname.text,
-                              "phone": _phone.text,
-                              "email": _email.text,
-                              "url": _url.text,
-                              "photo":
-                              "https://scontent.fcrk1-4.fna.fbcdn.net/v/t39.30808-6/470820644_921287969980813_6651135653347092985_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHmDNNRcYUMcQE2wBKXvV9g8y0D0Oi_9DbzLQPQ6L_0NtMTHR1wsDUcJPYZJ7DPfUf5hGyHmadFVnt_FXki-ki2&_nc_ohc=F2B8BM7Lc_QQ7kNvwGkYrYV&_nc_oc=AdlS_as0CEt-IlefJeu1Srezr2xRWLhAJVsSbzHWAEMuk2si42MhOPRVsoiUYUEdOrg&_nc_zt=23&_nc_ht=scontent.fcrk1-4.fna&_nc_gid=knZ-Cnt4b6B7T66g2Rvydg&oh=00_AfHmVHNfuIrm1jem8QwNBzzN0aRzsJHCJX6jspotl-MxGw&oe=67FFEC59",
-                            });
-                            _myBox.put('contacts', contacts);
-                            print(_myBox.get('contacts'));
-                          });
-
-                          _fname.text = "";
-                          _lname.text = "";
-                          _phone.text = "";
-                          _email.text = "";
-                          _url.text = "";
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                  message: Column(
-                    children: [
-                      Icon(
-                        CupertinoIcons.person_circle_fill,
-                        color: CupertinoColors.systemGrey,
-                        size: 200,
-                      ),
-                      CupertinoButton(
-                        child: Text('Add Photo'),
-                        onPressed: () {},
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            color: CupertinoColors.systemGrey.withOpacity(0.1)),
-                        child: Column(
-                          children: [
-                            CupertinoTextField(
-                              controller: _fname,
-                              placeholder: 'First Name',
-                              decoration: BoxDecoration(
-                                  color:
-                                  CupertinoColors.systemGrey.withOpacity(0.0)),
-                            ),
-                            Divider(
-                              color: CupertinoColors.systemGrey.withOpacity(0.2),
-                            ),
-                            CupertinoTextField(
-                              controller: _lname,
-                              placeholder: 'Last Name',
-                              decoration: BoxDecoration(
-                                  color:
-                                  CupertinoColors.systemGrey.withOpacity(0.0)),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      CupertinoTextField(
-                        controller: _phone,
-                        prefix: Icon(
-                          CupertinoIcons.add_circled_solid,
-                          color: CupertinoColors.systemGreen,
-                        ),
-                        placeholder: 'Add Phone',
-                        decoration: BoxDecoration(
-                            color: CupertinoColors.systemGrey.withOpacity(0.1)),
-                      ),
-                      SizedBox(height: 10),
-                      CupertinoTextField(
-                        controller: _email,
-                        prefix: Icon(
-                          CupertinoIcons.add_circled_solid,
-                          color: CupertinoColors.systemGreen,
-                        ),
-                        placeholder: 'Add Email',
-                        decoration: BoxDecoration(
-                            color: CupertinoColors.systemGrey.withOpacity(0.1)),
-                      ),
-                      SizedBox(height: 10),
-                      CupertinoTextField(
-                        controller: _url,
-                        prefix: Icon(
-                          CupertinoIcons.add_circled_solid,
-                          color: CupertinoColors.systemGreen,
-                        ),
-                        placeholder: 'Add url',
-                        decoration: BoxDecoration(
-                            color: CupertinoColors.systemGrey.withOpacity(0.1)),
-                      ),
-                      SizedBox(height: double.maxFinite)
-                    ],
-                  ),
-                );
+        middle: _isSearching ? null : Text('Contacts'),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(CupertinoIcons.add),
+              onPressed: () {
+                _openContactSheet();
               },
-            );
-          },
+            ),
+          ],
         ),
       ),
       child: SafeArea(
@@ -882,6 +791,7 @@ setState(() {
               ),
               SizedBox(height: 15),
               CupertinoTextField(
+                controller: _searchController,
                 placeholder: 'Search',
                 decoration: BoxDecoration(
                     color: CupertinoColors.systemGrey.withOpacity(0.2),
@@ -894,7 +804,24 @@ setState(() {
                     size: 20,
                   ),
                 ),
-                suffix: Padding(
+                suffix: _searchController.text.isNotEmpty
+                    ? GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _searchController.clear();
+                      _filterContacts();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(
+                      CupertinoIcons.clear_circled_solid,
+                      color: CupertinoColors.systemGrey,
+                      size: 20,
+                    ),
+                  ),
+                )
+                    : Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Icon(
                     CupertinoIcons.mic_fill,
@@ -941,31 +868,113 @@ setState(() {
                 child: ListView.builder(
                     itemCount: contacts.length,
                     itemBuilder: (context, int index) {
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            name = contacts[index]['name'];
-                            phone = contacts[index]['phone'];
-                            url = contacts[index]['url'];
-                            email = contacts[index]['email'];
-                            photo = contacts[index]['photo'];
-                          });
-                          Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                  builder: (context) => Contact()));
-                        },
-                        child: Container(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(contacts[index]['name'] == " "
-                                  ? contacts[index]['phone']
-                                  : contacts[index]['name']),
-                              Divider(
-                                color: CupertinoColors.systemGrey.withOpacity(0.3),
+                      // Using CupertinoSwipeAction for iOS-style swipe delete
+                      return Dismissible(
+                        key: UniqueKey(),
+                        background: Container(
+                          alignment: Alignment.centerRight,
+                          color: CupertinoColors.destructiveRed,
+                          child: Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Text(
+                              'Delete',
+                              style: TextStyle(
+                                color: CupertinoColors.white,
+                                fontWeight: FontWeight.bold,
                               ),
-                            ],
+                            ),
+                          ),
+                        ),
+                        direction: DismissDirection.endToStart, // Only allow right to left (iOS style)
+                        confirmDismiss: (direction) async {
+                          // Show a confirmation dialog (iOS style)
+                          return await showCupertinoDialog<bool>(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return CupertinoAlertDialog(
+                                title: Text('Delete Contact'),
+                                content: Text('Are you sure you want to delete this contact?'),
+                                actions: <Widget>[
+                                  CupertinoDialogAction(
+                                    child: Text('Cancel'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop(false);
+                                    },
+                                  ),
+                                  CupertinoDialogAction(
+                                    isDestructiveAction: true,
+                                    child: Text('Delete'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop(true);
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        onDismissed: (direction) {
+                          _deleteContact(index);
+                        },
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              name = contacts[index]['name'];
+                              phone = contacts[index]['phone'];
+                              // Check if phoneNumbers exist and pass them too
+                              if (contacts[index]['phoneNumbers'] != null) {
+                                phoneNumbers = contacts[index]['phoneNumbers'];
+                              } else {
+                                // For backward compatibility with existing contacts
+                                phoneNumbers = [{'label': 'mobile', 'number': contacts[index]['phone']}];
+                              }
+                              url = contacts[index]['url'];
+                              email = contacts[index]['email'];
+                              photo = contacts[index]['photo'];
+                              // Pass the flag to indicate if it's a base64 image
+                              isBase64 = contacts[index]['isBase64'] ?? false;
+                            });
+                            Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) => Contact()));
+                          },
+                          child: Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    // Show a small thumbnail of the contact photo
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 10.0),
+                                      child: contacts[index]['isBase64'] == true
+                                          ? ClipRRect(
+                                        borderRadius: BorderRadius.circular(15),
+                                        child: Image.memory(
+                                          base64Decode(contacts[index]['photo']),
+                                          width: 30,
+                                          height: 30,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      )
+                                          : CircleAvatar(
+                                        radius: 15,
+                                        backgroundImage: NetworkImage(
+                                          contacts[index]['photo'],
+                                        ),
+                                      ),
+                                    ),
+                                    Text(contacts[index]['name'] == " "
+                                        ? contacts[index]['phone']
+                                        : contacts[index]['name']),
+                                  ],
+                                ),
+                                Divider(
+                                  color: CupertinoColors.systemGrey.withOpacity(0.3),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
